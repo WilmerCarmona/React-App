78ec26e9a4b0dbbc24d964dafafadf901763403a
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-07-20 22:42:46 -0500

    Update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Testimonio from "./componentes/Testimonio";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Testimonio />
+    </div>
+  );
 }
 
 export default App;
---
+++ my-app/src/componentes/Testimonio.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Testimonio() {
+  return (
+    <div className="contenedor-testimonio">
+      <img
+        className="imagen-testimonio"
+        src={require("../imagenes/testimonio-Billie.jpg")}
+        alt="imagenBillie"
+      />
+      <div clasName="contenedor-p-testimonio">
+        <p className="nombre-testimonio">Billie Eilish</p>
+        <p classname="cargo-testimonio">Cantante</p>
+        <p className="texto-testimonio">
+          Me Gusta esta pagina, está bien diseñada
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonio;
