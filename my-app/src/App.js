60b31d2ff619e919d7e1a0111bf9792cf7b733f0
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-07-21 20:12:54 -0500

    Update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,7 @@ import Testimonio from "./componentes/Testimonio";
 function App() {
   return (
     <div className="App">
-      <div>
+      <div className="contenedor-principal">
         <h1 className="tit-testimonio">Reseñas de las personas</h1>
         <Testimonio />
       </div>
