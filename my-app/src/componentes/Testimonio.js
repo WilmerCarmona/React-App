8bc1157e49b21917444aac77d57f50f6a2a0c523
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-07-22 19:10:40 -0500

    update

--- my-app/src/componentes/Testimonio.js
+++ my-app/src/componentes/Testimonio.js
@@ -11,7 +11,11 @@ function Testimonio() {
         <p className="nombre-testimonio">Billie Eilish</p>
         <p className="cargo-testimonio">Cantante</p>
         <p className="texto-testimonio">
-          Me Gusta esta pagina, está bien diseñada
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+          odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
+          quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent
+          mauris. Fusce nec tellus sed augue semper porta. Mauris massa.
+          Vestibulum lacinia arcu eget nulla.
         </p>
       </div>
     </div>
