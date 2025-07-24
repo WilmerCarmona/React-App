d1625e8749b385c81cfdaf736427779fb73fcef4
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-07-23 19:07:40 -0500

    Update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,17 @@ function App() {
     <div className="App">
       <div className="contenedor-principal">
         <h1 className="tit-testimonio">Reseñas de las personas</h1>
-        <Testimonio />
+        <Testimonio
+          nombre="Billie Eilish"
+          pais="California"
+          imagen=""
+          cargo="Cantante y compositor"
+          empresa="OpenMusic"
+          texto=" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. 
+                  Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at 
+                  nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec 
+                  tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla."
+        />
       </div>
     </div>
   );
