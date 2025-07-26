8e27e9d14b7e9bf498d667cfd6d1cca2be82a63d
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-07-25 19:20:31 -0500

    Update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,6 +18,18 @@ function App() {
                   tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla."
           fecha="24/07/2025"
         />
+        <Testimonio
+          nombre="Lana Del Rey"
+          pais="Florida"
+          imagen="Lana"
+          cargo="Cantante y compositor"
+          empresa="Spotify"
+          texto=" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. 
+                  Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at 
+                  nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec 
+                  tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla."
+          fecha="25/07/2025"
+        />
       </div>
     </div>
   );
