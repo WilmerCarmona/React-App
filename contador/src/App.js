14064b36e21af387123f5eb04217af943e1e25b7
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-08-02 21:27:06 -0500

    Update App.js

--- contador/src/App.js
+++ contador/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img className="logo" src={logoNav} alt="Logo" />
       </div>
       <div className="contenedor-principal">
-        <Boton texto="Clic" botonClic={true} />
+        <Boton texto="Clic" botonClic={true} manejarClic={} />
       </div>
     </div>
   );
