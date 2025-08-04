c4b39c2612ed70a3efd05f1c90b21c10d1860248
Tamarindosexy <wilmercarmonawork@gmail.com>
2025-08-03 20:51:23 -0500

    Update App.js

--- contador/src/App.js
+++ contador/src/App.js
@@ -3,14 +3,17 @@ import Boton from "./componentes/Boton";
 import logoNav from "./imagenes/logoNav.png";
 
 function App() {
+  const manejarClic = () => {
+    console.log('Click');
+  }
   return (
     <div className="App">
       <div class="logo-contenedor">
         <img className="logo" src={logoNav} alt="Logo" />
       </div>
       <div className="contenedor-principal">
-        <Boton texto="clic" botonClic={true} manejarClic={} />
-        <Boton texto="reiniciar" botonClic={false} manejarClic={} />
+        <Boton texto="Click" botonClic={true} manejarClic={} />
+        <Boton texto="Reiniciar" botonClic={false} manejarClic={} />
       </div>
     </div>
   );
